2000a2e0e87d5a363b9e82a9edad72489643d60b
Gabriel Trahan <godmaster017@gmail.com>
2023-03-24 14:19:30 -0400

    router_base

--- src/main.tsx
+++ src/main.tsx
@@ -16,11 +16,14 @@ import { createPageBox } from './components/PageBox';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<App />}>
-      <Route index path='./' element={createPageBox('game_title', <GamePage />)} />
-      <Route index path='./history' element={createPageBox('history_title', <HistoryPage />)} />
-      <Route index path='./settings' element={createPageBox('settings_title', <SettingsPage />)} />
+      <Route index path='/' element={createPageBox('game_title', <GamePage />)} />
+      <Route index path='/history' element={createPageBox('history_title', <HistoryPage />)} />
+      <Route index path='/settings' element={createPageBox('settings_title', <SettingsPage />)} />
     </Route>
-  )
+  ),
+  {
+    basename: "/monchou_dice/"
+  }
 );
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
