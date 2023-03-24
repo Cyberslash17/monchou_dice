3048a37c26399e2f202f89f8d701977966305d4c
Gabriel Trahan <godmaster017@gmail.com>
2023-03-24 14:13:30 -0400

    base2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'monchou_dice/'
+  base: '/monchou_dice/'
 });
